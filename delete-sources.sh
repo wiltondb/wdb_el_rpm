f3f2577918e82a75e09767237050dc8de8643072
alex <alex@staticlibs.net>
2023-01-07 14:28:29 +0000

    update to 2.2.1

--- delete-sources.sh
+++ delete-sources.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-VERSION_BABEL=2_2_0
+VERSION_BABEL=2_2_1
 VERSION_PG=14_5
 
-rm ./BABEL_${VERSION_BABEL}.tar.gz
+rm -f ./BABEL_${VERSION_BABEL}.tar.gz
 touch ./BABEL_${VERSION_BABEL}.tar.gz
-rm ./BABEL_${VERSION_BABEL}__PG_${VERSION_PG}.tar.gz
+rm -f ./BABEL_${VERSION_BABEL}__PG_${VERSION_PG}.tar.gz
 touch ./BABEL_${VERSION_BABEL}__PG_${VERSION_PG}.tar.gz
